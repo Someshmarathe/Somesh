a5ea25f307132a2fdd99f0265d37ed5dd1566456
someshmarathe <someshmarathe00@gamil.com>
2026-05-23 00:44:50 +0530

    add in sout

--- newCode.java
+++ newCode.java
@@ -3,6 +3,6 @@ package TestGit;
 public class newCode {
     public static void main(String[] args) {
         System.out.println("hello java");
-        System.out.println("Added by somu hello java");
+        System.out.println("Added by somu hello java 123");
     }
 }
