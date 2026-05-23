bea632404fc2dac5578d156fedc87f34868998ea
someshmarathe <someshmarathe00@gamil.com>
2026-05-23 08:22:33 +0530

    added by somu

--- newCode.java
+++ newCode.java
@@ -4,5 +4,6 @@ public class newCode {
     public static void main(String[] args) {
         System.out.println("hello java");
         System.out.println("Added by somu hello java 123");
+        System.out.println("Added by Somu");
     }
 }
